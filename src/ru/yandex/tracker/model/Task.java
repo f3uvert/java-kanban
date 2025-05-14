83d6271de7fe203b62719d276622199d3d1c3a3c
f3uvert <geradaividov@gmail.com>
2025-05-15 00:53:16 +0300

    6_sprint_task_develop_branch

--- src/ru/yandex/tracker/model/Task.java
+++ src/ru/yandex/tracker/model/Task.java
@@ -1,8 +1,6 @@
 package ru.yandex.tracker.model;
-
-import ru.yandex.tracker.service.FileBackedTaskManager;
 import ru.yandex.tracker.service.TaskPriority;
-import ru.yandex.tracker.service.TaskType;
+
 
 import java.util.Objects;
 
